3bde602f9c7224307e6c100de23b44c6ffb398fb
Jen Ayala <jenayala1@gmail.com>
2017-07-13 16:55:17 -0500

    Continued revisions on styling to HTML, CSS & JS, pending correction to navbar

--- js/ja-js.js
+++ js/ja-js.js
@@ -2,15 +2,6 @@
     "use strict";
 
 
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-}
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
@@ -21,7 +12,14 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
-
+$('#header1').click(function(){
+		$('header1').animate({
+			"font-size": "200px", ///increases the elements size &
+			"opacity": 0 //fade out the element to invisible 
+		}, 1500, function(){ ///time designated for the 1st function, followed by the call of a new function to
+			window.location = "https.google,com" //-redirect the page to the link
+		});
+	});
 
 
 
